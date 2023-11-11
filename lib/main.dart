b422aeae2f9ce69c8c4a13274d93b38e1b64aaf9
มาสเตอร์ อึ่ง <phrombutr@gmail.com>
2023-11-11 17:28:26 +0700

    finish 11nov

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:banchaproj/states/spalash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  HttpOverrides.global = MyHttpOverride();
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,5 +18,13 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       home: SplashScreen(),
     );
+  }
+}
+
+class MyHttpOverride extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (cert, host, port) => true;
   }
 }
